0e06cb635b9aa45751de4b4a27471d56ac1ecae5
Ameen0001 <ameenappu2002@gmail.com>
2024-01-30 11:39:34 -0800

    initialcommit

---
+++ hello task.py
@@ -0,0 +1,28 @@
+s=" kerala gods / on country"
+r="India"
+print(s)
+print(s.upper())
+print(s.lower())
+print(s.strip())
+print(len(s))
+print(s.split("/"))
+print(s.replace("on","in"))
+print(s[4:13])
+j=s+" "+r
+print(j)
+
+x,y,z="red","green","blue"
+print(x)
+print(y)
+print(z)
+x=y=z="colour"
+print(x)
+print(y)
+print(z)
+colour=["red","green","blue"]
+x,y,z=colour
+print(x)
+print(y)
+print(z)
+x="i love my country india is my country"
+print(x.count("country"))
---
+++ hello.py
@@ -0,0 +1,61 @@
+print("hello world") #print
+a=100
+print(a)
+print(200)
+x,y,z ="apple","cherry","orange"
+print(x)
+print(y)
+print(z)
+x=y=z="good"
+print(x,y,z)
+fruits=["apple","cheery","orange"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+
+b=5
+c=13
+d=3+4j
+print(type(b))
+print(type(c))
+print(type(d))
+print(int(b))
+print(float(c))
+
+
+import random
+print(random.randrange(100,130))
+
+
+print("akash")
+e=" malay,alam "
+print(e)
+print(e[0])
+print(e[:4])
+print(e[3:])
+print(e[1:4])
+print(len(e))
+
+print(e.upper())
+print(e.lower())
+print(e.strip())
+
+print(e.replace("y","A"))
+print(e.split(","))
+f="my"
+g="name"
+h=f+" "+g
+print(h)
+
+
+
+
+
+
+
+
+
+
+
